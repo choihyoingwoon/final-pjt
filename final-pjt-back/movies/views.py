4d5ffee3d04242e03f8c2a7c6e707590662b9308
chaeeun5 <jce0497@gmail.com>
2022-11-16 15:36:03 +0900

    fixtures

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Genre, Top_Movie, Now_Movie
+from .serializers import TopMovieSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+def index(request):
+    movies = Top_Movie.objects.all()
+    serializer = TopMovieSerializer(movies, many=True)
+    return Response(serializer.data)
