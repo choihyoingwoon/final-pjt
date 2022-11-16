2f0c3ff32c0c7f21b45df72511bec6572f1cb774
최형운 <cpobo@hanmail.net>
2022-11-16 17:33:16 +0900

    front home(moviecard)

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
-from .models import Top_Movie
+from .models import Top_Movie, Now_Movie
 
 
 class TopMovieSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Top_Movie
-        fields = '__all__'
+        fields = '__al__'
 
-# class NowMovieSerializer(serializers.ModelSerializer):
+class NowMovieSerializer(serializers.ModelSerializer):
     
-#     class Meta:
-#         model = Now_Movie
-#         fields = '__al__'
+    class Meta:
+        model = Now_Movie
+        fields = '__al__'
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-from .models import Top_Movie
+from .models import Genre, Top_Movie, Now_Movie
 from .serializers import TopMovieSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
 
 # Create your views here.
-@api_view(['GET'])
 def index(request):
     movies = Top_Movie.objects.all()
     serializer = TopMovieSerializer(movies, many=True)
