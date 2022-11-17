3b4fd382c6719aeb207f5ea8a2e69c0dc35a6635
chaeeun5 <jce0497@gmail.com>
2022-11-17 17:26:08 +0900

    community

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from .models import Top_Movie, Now_Movie
 
-
 class TopMovieSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Top_Movie
         fields = '__all__'
